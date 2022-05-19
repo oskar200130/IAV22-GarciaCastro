68cfe9a99d17336811e81f6ef73a0713706b10c3
oscar <osgarc05@ucm.es>
2022-05-19 12:11:21 +0200

    Inicio granjero y GameManager

---
+++ GranjaFrijoles/Assets/Granjero/IsDay.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class IsDay : Conditional
+{
+	public override TaskStatus OnUpdate()
+	{
+		return TaskStatus.Success;
+	}
+
+	public bool isDay()
+    {
+		return true;
+    }
+}
