a4e436a0c5021ec01364a9e7580a104be6ce1ffd
Unknown <acarni01@ucm.es>
2022-05-22 01:13:17 +0200

    Gallina se lleva la comida
    A veces funciona regu con el agente

---
+++ GranjaFrijoles/Assets/IA_HUERTO/Paja_Interact.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paja_Interact : MonoBehaviour
+{
+    bool sePuedePisar = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Comprobamos si ha entrado el pollo
+        if(other.GetComponent<PolloInteract>())
+        {
+            other.GetComponent<PolloInteract>().sePuedeCogerComida = true;
+            other.GetComponent<PolloInteract>().setComida(transform.parent);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        // Comprobamos si ha entrado el pollo
+        if (other.GetComponent<PolloInteract>())
+        {
+            other.GetComponent<PolloInteract>().sePuedeCogerComida = false;
+        }
+    }
+}
--- GranjaFrijoles/Assets/Pollo/PolloInteract.cs
+++ GranjaFrijoles/Assets/Pollo/PolloInteract.cs
@@ -8,15 +8,18 @@ public class PolloInteract : MonoBehaviour
     public bool sePuedePisar;
     public bool sePuedeAbrir;
     public bool sePuedeCogerComida;
+    bool conComida = false;
 
     // Referencia al Huerto
     [SerializeField]
     Huerto_Behaviour huerto;
 
+    // Para guardarnos la comida
+    Transform paja;
+
 
     void Start()
     {
-        
     }
 
     void Update()
@@ -35,5 +38,32 @@ public class PolloInteract : MonoBehaviour
                 Debug.Log("Lohepisao");
             }
         }
+        else if(sePuedeCogerComida)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                conComida = !conComida;
+                if (!conComida)
+                    cogerComida();
+                else
+                    soltarComida();
+            }
+        }
+    }
+
+    void cogerComida()
+    {
+        paja.SetParent(transform);
+        Debug.Log("Man");
+    }
+    void soltarComida()
+    {
+        paja.SetParent(null);
+        paja = null;
+        Debug.Log("Man");
+    }
+    public void setComida(Transform com)
+    {
+        paja = com;
     }
 }
